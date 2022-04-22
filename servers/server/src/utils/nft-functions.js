ff29b16ab5758c1b5ea70cf548de5c7d2d35ef82
Sutanto <sutanto4@illinois.edu>
2022-04-22 01:29:15 -0500

    list

--- servers/server/src/utils/nft-functions.js
+++ servers/server/src/utils/nft-functions.js
@@ -221,6 +221,7 @@ export async function mintMarketItem(url, signer, amount, royalty) {
 
  export async function listMarketItem(nftId, lister, account, price, useGco,amount) {
   // verify signer
+  console.log(amount)
   const signedNFT = ERC1155NFT.connect(lister)
   const signedNFTMarket = ERC1155Market.connect(lister)
   const signedGiesCoin = GiesCoin.connect(lister)
