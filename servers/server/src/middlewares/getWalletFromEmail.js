b68b4df5a5f5cad023ac215c99f6329a688151df
Jongwon Park <jwparktom@gmail.com>
2022-04-27 15:25:13 -0500

    Update summit endpoint

--- servers/server/src/middlewares/getWalletFromEmail.js
+++ servers/server/src/middlewares/getWalletFromEmail.js
@@ -1,6 +1,6 @@
 import httpStatus from 'http-status'
 
-import { utils } from 'ethers'
+import { ethers, utils } from 'ethers'
 import { Wallets } from '../models'
 import { ApiError } from '../utils'
 
@@ -24,14 +24,21 @@ export default function getWalletFromEmail() {
     const walletRecords = await Wallets.findAll(queryParams)
 
     if (!walletRecords || walletRecords.length === 0) {
-      // no wallet exists
-      return next(new ApiError(httpStatus.BAD_REQUEST, `Wallet for user ${email} not found`))
+      // no wallet exists, create one
+      // return next(new ApiError(httpStatus.BAD_REQUEST, `Wallet for user ${email} not found`))
+      const wallet = ethers.Wallet.createRandom()
+
+      // Store New Wallet Data (wallet.address is address 0 - `m/44'/60'/0'/0/0`)
+      const createParams = { email, main_address: wallet.address, seed_phrase: wallet.mnemonic.phrase }
+      await Wallets.create(createParams)
+      req.user.wallet = wallet
+    } else {
+      // wallet exists, grab the wallet data & append that wallet to `req.user`
+      const walletData = walletRecords[0]
+      const wallet = utils.HDNode.fromMnemonic(walletData.seed_phrase)
+      req.user.wallet = wallet
     }
 
-    // wallet exists, grab the wallet data & append that wallet to `req.user`
-    const walletData = walletRecords[0]
-    const wallet = utils.HDNode.fromMnemonic(walletData.seed_phrase)
-    req.user.wallet = wallet
     return next()
   }
 }
