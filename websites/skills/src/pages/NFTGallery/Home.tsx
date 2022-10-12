7a975d5242a7756407cc5a39b35e2fb0ec4e14e8
stangri2 <stangri2@illinois.edu>
2022-10-12 18:38:59 -0500

    changed images for skills

--- websites/skills/src/pages/NFTGallery/Home.tsx
+++ websites/skills/src/pages/NFTGallery/Home.tsx
@@ -1,66 +1,96 @@
 import React, { useState } from 'react'
-import image from '../../assets/1.png'
+import pythonImage from '../../assets/python.jpg'
+import javaImage from '../../assets/java.jpeg'
+import leadershipImage from '../../assets/leadership.jpeg'
+import communicationImage from '../../assets/communication.png'
+import excelImage from '../../assets/excel.png'
+import gitImage from '../../assets/git.png'
+import postmanImage from '../../assets/postman.jpeg'
+import powerpointImage from '../../assets/powerpoint.webp'
+import mathImage from '../../assets/math.jpeg'
+import quantImage from '../../assets/quantumcomputing.webp'
+import cyberImage from '../../assets/cyber.jpeg'
+import datascienceImage from '../../assets/datascience.png'
+import aiImage from '../../assets/ai.jpeg'
+import hardworkImage from '../../assets/hardwork.jpeg'
+import calculusImage from '../../assets/calc.png'
 
 const arr = [
   {
     name: 'Leadership',
     description: 'Description',
+    image: leadershipImage,
   },
   {
-    name: 'C++',
+    name: 'Java',
     description: 'Description',
+    image: javaImage,
   },
   {
     name: 'Python',
     description: 'Description',
+    image: pythonImage,
   },
   {
     name: 'Communication',
     description: 'Description',
+    image: communicationImage,
   },
   {
-    name: 'Empathy',
+    name: 'Calculus',
     description: 'Description',
+    image: calculusImage,
   },
   {
     name: 'Hard Work',
     description: 'Description',
+    image: hardworkImage,
   },
   {
     name: 'Excel',
     description: 'Description',
+    image: excelImage,
   },
   {
     name: 'Git',
     description: 'Description',
+    image: gitImage,
   },
   {
     name: 'Postman',
     description: 'Description',
+    image: postmanImage,
   },
   {
     name: 'Powerpoint',
     description: 'Description',
+    image: powerpointImage,
   },
   {
     name: 'Discrete Structures',
     description: 'Description',
+    image: mathImage,
   },
   {
     name: 'Artifiical Intelligence',
     description: 'Description',
+    image: aiImage,
   },
   {
     name: 'Data Science',
     description: 'Description',
+    image: datascienceImage,
   },
   {
     name: 'Cybersecurity',
     description: 'Description',
+    image: cyberImage,
+
   },
   {
     name: 'Quantum Computing',
     description: 'Description',
+    image: quantImage,
   },
 ]
 
@@ -79,7 +109,7 @@ export default function GalleryHome() {
     })
     .map((data) => (
       <div key={data.name} className="w-1/3 h-1/15 ">
-        <img src={image} alt="Avatar" />
+        <img src={data.image} alt="Avatar" />
         <div className="text-center">
           <h4>
             <b>{data.name}</b>
